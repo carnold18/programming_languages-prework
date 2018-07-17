caca57b1e6234346719321d8098c1b2ddcf6efab
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-17 03:27:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,14 @@
+require pry
+
 def reformat_languages(languages)
   new_hash = {}
-    languages.each do |style, hash|
+    languages.each do |styles, hash|
       hash.each do |langauge, type|
         if new_hash[langauge] == nil
           new_hash[langauge] = type
-          new_hash[langauge][:style] = [style]
+          new_hash[langauge][:style] = [styles]
         else
-          new_hash[langauge][:style].push(style)
+          new_hash[langauge][:style].push(styles)
         end
       end
     end
